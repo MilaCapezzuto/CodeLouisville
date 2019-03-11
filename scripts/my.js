3589f46baa6a28caa144801cb4ecf4fc6a8465fd
MilaCapezzuto <Radmilka32@gmail.com>
2019-03-11 19:16:23 -0400

    adding the clock

--- scripts/my.js
+++ scripts/my.js
@@ -1,5 +1,23 @@
 // A $( document ).ready() block.
 
+// Add clock to website
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+//$(document).ready(function(){
 $ ("#submitButton").click(function(){
 alert("Thank you for subscribing!")
 })
